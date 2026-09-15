6b09603fbabecd3b88e689060936092ec4c7d692
Yarden <yardenextra@gmail.com>
2026-09-15 21:06:50 +0300

    Deploy GrantMatch build to GitHub Pages

--- src/rtlCache.ts
+++
@@ -1,8 +0,0 @@
-import createCache from '@emotion/cache'
-import rtlPlugin from 'stylis-plugin-rtl'
-import { prefixer } from 'stylis'
-
-export const rtlCache = createCache({
-  key: 'muirtl',
-  stylisPlugins: [prefixer, rtlPlugin],
-})
